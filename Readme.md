facf0a9f2988ac8de195035b062fd9d5fae0b20d
Ryan Schmukler <rschmukler@gmail.com>
2013-09-30 15:17:02 -0400

    Added support for multi-attribute slugs

--- Readme.md
+++ Readme.md
@@ -17,11 +17,38 @@ On the server (with node.js):
 ```js
 var slug = require('slug');
 
-var blog = model('blog')
+var Blog = model('blog')
   .attr('title')
   .attr('content')
   .use(slug('title'))
+
+  var b = new Blog();
+  b.title('A seriously interesting title');
+  b.slug();
+  // => 'a-seriously-interesting-title'
+
 ```
+
+## Complex Example
+
+```js
+var slug = require('slug');
+
+var Blog = model('blog')
+  .attr('title')
+  .attr('author')
+  .attr('content')
+  .use(slug(['title', 'author'], ':title-by-:author))
+
+  var b = new Blog();
+  b.title('Some blog post');
+  b.author('Ryan S.');
+
+  b.slug();
+  // => 'some-blog-post-by-ryan-s'
+
+```
+
 
 ## License
 
--- index.js
+++ index.js
@@ -14,12 +14,36 @@ var slug = require('./deps/slug');
  *    .use(slug('title'));
  */
 
-module.exports = function(attr) {
+module.exports = function(attrs, format) {
+  if(typeof attrs == 'string')
+    attrs = [attrs];
+
+  if(format === undefined) {
+    format = '';
+    for(var i = 0; i < attrs.length; ++i) {
+      format += ':' + attrs[i] + '-';
+    }
+    format = format.slice(0, -1);
+  }
+
+  var makeSlugString = function(obj) {
+    var string = format;
+    var toSub = format.match(/(:\w+)/g);
+    toSub.forEach(function(sub) {
+      var attr = sub.slice(1),
+          val = obj[attr]() || '';
+      string = string.replace(sub, val);
+    });
+    obj.slug(slug(string));
+  };
+
   return function(model) {
     model.attr('slug', { type : 'string' });
 
     // bind given `attr` changes to slug changes
-    model.on('change:' + attr, function(obj, val) { obj.slug(slug(val)); });
+    attrs.forEach(function(attr) {
+      model.on('change:' + attr, makeSlugString);
+    });
 
     // create a slug if it's not set manually and if the model is new
     model.on('saving', function(obj) {
